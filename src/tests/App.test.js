6ab7e5bc3f7297e327c0b02f0cd9d0ee16a66cd4
Valéria <valeriamenezes022@gmail.com>
2022-09-16 10:32:43 -0300

    exercícios de teste do Wolf finalizados

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,24 +1,40 @@
 import { render, screen } from '@testing-library/react';
 import App from '../App';
+import mocks from './mocks';
 
 describe('Test Rick & Morty API', () => {
 
   beforeEach(()=>{
-    //Fazer o mock do fetch aqui 
+     global.fetch = jest.fn(() => Promise.resolve({
+       json: () => Promise.resolve(mocks)
+     }))
     
     render(<App/>)
   })
   
   test('Verifica se aparece o card com titulo de "Rick Sanchez"', () => {
-
-  })
+    const characterName = screen.getByRole('heading', {
+      name: /rick sanchez/i,
+      level: 3,
+    })
+    expect(characterName).toBeInTheDocument();
+})
 
   test('Verifica se existem o input de texto e o botão "Buscar"', () => {
-    
+    const input = screen.getByRole('textbox');
+    const btn = screen.getByRole('button', {
+      name: /buscar/i,
+    })
+    expect(input).toBeInTheDocument();
+    expect(btn).toBeInTheDocument();
   })
 
   test('Verifica se ao buscar por "Smith" aparecem 4 cards', () => {
-    
+    const quantityName = screen.getAllByRole('heading', {
+      name: / * smith/i,
+      level: 3,
+    })
+    expect(quantityName).toHaveLength(4);
   })
 
 })
